c79ad3f31f4fe4c65e75538696836da52ec37426
leuel-a <leuela1993@gmail.com>
2024-12-08 11:19:09 +0300

    feat: add correlation analysis for all the datasets

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 MONTHS = [
@@ -25,6 +26,7 @@ def plot_bar_chart(df: pd.DataFrame, column: str, title: str = '') -> None:
     plt.title(title)
     plt.xlabel('Months')
     plt.ylabel(f'{column}')
+
     plt.show()
 
 
@@ -46,4 +48,26 @@ def plot_line_chart(df: pd.DataFrame, column: str, title: str = '') -> None:
     plt.title(title)
     plt.xlabel('Months')
     plt.ylabel(f'{column}')
+
+    plt.show()
+
+def plot_heatmap(correlation_matrix: pd.DataFrame, title: str) -> None:
+    """
+    Plots a heatmap for the given correlation matrix.
+
+    Parameters:
+    correlation_matrix (pd.DataFrame): The correlation matrix to plot.
+
+    Returns:
+    None
+    """
+    if not isinstance(correlation_matrix, pd.DataFrame):
+        raise ValueError('correlation_matrix must be a pandas.DataFrame')
+
+    plt.figure(figsize=(20, 8))
+
+    # here it uses seaborn to plot the heatmap
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f')
+    plt.title(title)
+
     plt.show()
